6e80e59bee258b60dd7b5aae2ee3e4b9b26c9d75
higor Santos <higoraugusto120@gmail.com>
2024-03-25 10:55:03 -0300

    removendo arquivos

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -7,24 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('/admin')->group( function () {
-    Route::prefix('/employee')->group( function () {
-        Route::get('', [EmployeeController::class, 'index'])->name('employees.index');
-        Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
-        Route::post('', [EmployeeController::class, 'store'])->name('employees.store');
-        Route::get('/{employee}', [EmployeeController::class, 'show'])->name('employees.show');
-        Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
-        Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
-        Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
-    });
-    Route::prefix('/customer')->group( function () {
-        Route::get('', [CustomerController::class, 'index'])->name('customers.index');
-        Route::get('/create', [CustomerController::class, 'create'])->name('customers.create');
-        Route::post('', [CustomerController::class, 'store'])->name('customers.store');
-        Route::get('/{customer}', [CustomerController::class, 'show'])->name('customers.show');
-        Route::get('/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
-        Route::put('/{customer}', [CustomerController::class, 'update'])->name('customers.update');
-        Route::delete('/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
-    });
-});
