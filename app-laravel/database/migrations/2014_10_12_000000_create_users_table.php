80f8a1973704a5a64ed0a7bacabc17176f00af77
higor Santos <higoraugusto120@gmail.com>
2024-04-01 21:00:53 -0300

    subindo seeder

--- app-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ app-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('id')->id();
+            $table->id();
             $table->string('nome');
             $table->string('apelido');
             $table->string('celular');
--- app-laravel/database/seeders/DatabaseSeeder.php
+++ app-laravel/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\MatchSoccer;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +18,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = \Faker\Factory::create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $i = 1;
+        while ($i <= 5) {
+            DB::table('users')->insert([
+                'nome' => $faker->name(),
+                'apelido' => $faker->name(),
+                'celular' => $faker->phoneNumber(),
+                'dt_nasc' => $faker->date(),
+                'papel' => rand(1,2),
+                'sexo' => $this->getSexo(),
+                'email' => $faker->email(),
+                'password' => Hash::make('password'),
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon()
+            ]);
+            $i++;
+        }
+
+        $users = DB::table('users')->select(['id'])->get();
+
+        foreach($users as $user){
+            DB::table('matches_Soccer')->insert([
+                'user_id' => $user->id,
+                'status' => $this->getStatus(),
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon()
+            ]);
+        }
+    }
+    function getSexo()
+    {
+        $array = array(User::SEXO_MASCULINO, User::SEXO_FEMININO, User::SEXO_INDEFINIDO);
+        return $array[array_rand($array)];
+    }
+
+    function getStatus()
+    {
+        $array = array(MatchSoccer::STATUS_CREATED, MatchSoccer::STATUS_FINISHED, MatchSoccer::STATUS_IN_PROGRESS);
+        return $array[array_rand($array)];
     }
 }
